e71357318a66e00eba6d17ed70cfa280cac5d159
Anatoliy <mafik_tolik@mail.ru>
2023-12-26 20:22:51 +0300

    hw4_databases_1

---
+++ src/main/java/hw4_databases/App.java
@@ -0,0 +1,134 @@
+package hw4_databases;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        createDatabase(sessionFactory);
+        insertCourse(sessionFactory);
+        readCourse(sessionFactory);
+        updateCourse(sessionFactory);
+        readCourse(sessionFactory);
+        deleteCourse(sessionFactory);
+        readCourse(sessionFactory);
+
+        sessionFactory.close();
+    }
+
+    private static void createDatabase(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Create tables if not exists
+                session.createSQLQuery("CREATE TABLE IF NOT EXISTS courses (" +
+                        "id INT AUTO_INCREMENT PRIMARY KEY," +
+                        "title VARCHAR(255) NOT NULL," +
+                        "duration INT)").executeUpdate();
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.err.println("Ошибка в методе createDatabase");
+            }
+        }
+    }
+
+    private static void insertCourse(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Insert
+                Course course = new Course("Java Programming", 10);
+                session.save(course);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.err.println("Ошибка в методе insertCourse");
+            }
+        }
+    }
+
+    private static void readCourse(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Read
+                Course retrievedCourse = session.get(Course.class, 1L);
+                System.out.println("Retrieved Course: " + retrievedCourse);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.err.println("Ошибка в методе readCourse");
+            }
+        }
+    }
+
+    private static void updateCourse(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Update
+                Course retrievedCourse = session.get(Course.class, 1L);
+                retrievedCourse.setDuration(20);
+                session.update(retrievedCourse);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.err.println("Ошибка в методе updateCourse");
+            }
+        }
+    }
+
+    private static void deleteCourse(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            Transaction transaction = null;
+
+            try {
+                transaction = session.beginTransaction();
+
+                // Delete
+                Course retrievedCourse = session.get(Course.class, 1L);
+                session.delete(retrievedCourse);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                System.err.println("Ошибка в методе deleteCourse");
+            }
+        }
+    }
+}
---
+++ src/main/java/hw4_databases/Course.java
@@ -0,0 +1,65 @@
+package hw4_databases;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "duration")
+    private int duration;
+
+    public Course(Long id, String title, int duration) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Course(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
